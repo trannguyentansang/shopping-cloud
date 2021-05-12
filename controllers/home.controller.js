c5273df08ce9a99ba1eb4742de8f2b6741d74b30
Jack Edison <sangtntgcs190019@gmail.com>
2021-05-12 19:15:02 +0700

    add reports

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,5 +1,17 @@
 const User = require('../model/user.model')
+const Order = require('../model/order.model')
 module.exports.homePage =async (req, res)=>{
     var user = await User.findOne({_id:req.signedCookies.userId})
     res.render('index', {activepage: 'dashboard' ,layout: './layouts/common' , user:user})
+}
+module.exports.report =async (req, res)=>{
+    var user = await User.findOne({_id:req.signedCookies.userId})
+    res.render('report', {activepage: 'report' ,layout: './layouts/common' , user:user})
+}
+module.exports.postReport =async (req, res)=>{
+    var dateFrom = req.body.dateFrom
+    var dateTo = req.body.dateTo
+    var orders = await Order.find({date:{$gte: new Date(new Date(dateFrom).setHours(00, 00, 00)), $lt: new Date(new Date(dateTo).setHours(23, 59, 59))}})
+    var user = await User.findOne({_id:req.signedCookies.userId})
+    res.render('report', {activepage: 'report' ,layout: './layouts/common' , user:user, orders: orders, dateFrom:dateFrom, dateTo: dateTo})
 }
--- routes/home.route.js
+++ routes/home.route.js
@@ -6,6 +6,8 @@ var categoryController = require('../controllers/category.controller')
 var productController = require('../controllers/product.controller')
 
 router.get('/',middleware.requireAuth,middleware.checkPermissionManager, homeController.homePage)
+router.get('/report',middleware.requireAuth,middleware.checkPermissionManager, homeController.report)
+router.post('/get-report',middleware.requireAuth,middleware.checkPermissionManager, homeController.postReport)
 
 router.get('/cat-trash/search',middleware.requireAuth, middleware.checkIsProductManager, categoryController.trashSearch)
 router.get('/trash/search',middleware.requireAuth, middleware.checkIsProductManager, productController.trashSearch)
