138c848995d64506e7edf87f741bf5f964e64433
Jack Edison <sangtntgcs190019@gmail.com>
2021-05-18 15:48:55 +0700

    edit 400 error

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -14,35 +14,35 @@ module.exports.requireAuth = async (req, res, next)=>{
 }
 module.exports.checkPermissionManager = async (req, res, next)=>{
     if(res.locals.user.permission.perName==="CUSTOMER"){
-        res.status(400).send()
+        res.status(400).send('you do not have MANAGER role to do this action')
         return
     }
     next()
 }
 module.exports.checkIsCustomer = async (req, res, next)=>{
     if(res.locals.user.permission.perName!=="CUSTOMER"){
-        res.status(400).send()
+        res.status(400).send('you do not have CUSTOMER role to do this action')
         return
     }
     next()
 }
 module.exports.checkIsProductManager = async (req, res, next)=>{
     if(res.locals.user.permission.perName!=="PRODUCT_MANAGER"){
-        res.status(400).send()
+        res.status(400).send('you do not have PRODUCT MANAGER role to do this action')
         return
     }
     next()
 }
 module.exports.checkIsAdmin = async (req, res, next)=>{
     if(res.locals.user.permission.perName!=="ADMIN"){
-        res.status(400).send()
+        res.status(400).send('you do not have ADMIN role to do this action')
         return
     }
     next()
 }
 module.exports.checkIsOrderManager = async (req, res, next)=>{
     if(res.locals.user.permission.perName!=="ORDER_MANAGER"){
-        res.status(400).send()
+        res.status(400).send('you do not have ORDER MANAGER role to do this action')
         return
     }
     next()
